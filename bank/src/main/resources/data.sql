d1089fc3a39d11ecd92e2d7780a60d2031569f70
Marc-André Boudreault <marc-andre.boudreault@desjardins.com>
2020-04-18 12:16:18 -0400

    ajustement scripts SQL #7

--- bank/src/main/resources/data.sql
+++ bank/src/main/resources/data.sql
@@ -1,5 +1,8 @@
 
-INSERT INTO product (id, product_type, product_level, name) VALUES (1, 0, 0, 'ProduitA') ON CONFLICT DO NOTHING ;
-INSERT INTO product (id, product_type, product_level, name) VALUES (2, 1, 0, 'ProduitB') ON CONFLICT DO NOTHING ;
-INSERT INTO product (id, product_type, product_level, name) VALUES (3, 0, 1, 'ProduitC') ON CONFLICT DO NOTHING ;
-INSERT INTO product (id, product_type, product_level, name) VALUES (4, 1, 1, 'ProduitD') ON CONFLICT DO NOTHING ;
+INSERT INTO product (id, product_type, product_level, name) SELECT 1, 0, 0, 'ProduitA' WHERE NOT EXISTS ( SELECT id FROM product WHERE id = 1);
+
+INSERT INTO product (id, product_type, product_level, name) SELECT 2, 1, 0, 'ProduitB' WHERE NOT EXISTS ( SELECT id FROM product WHERE id = 2);
+
+INSERT INTO product (id, product_type, product_level, name) SELECT 3, 0, 1, 'ProduitC' WHERE NOT EXISTS ( SELECT id FROM product WHERE id = 3);
+
+INSERT INTO product (id, product_type, product_level, name) SELECT 4, 1, 1, 'ProduitD' WHERE NOT EXISTS ( SELECT id FROM product WHERE id = 4);
